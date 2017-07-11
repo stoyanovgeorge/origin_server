85b519814948fd907276c5890b8fbcff4236fee8
George Stoyanov <gstoyanov@gmail.com>
2017-07-11 12:56:34 +0000

    fixed a problem in the crontab.sh script

--- scripts/crontab.sh
+++ scripts/crontab.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-crontab -l $USERNAME | echo "* * * * * "sudo find /var/www/html/live/ -name "*.ts" -mmin +1 -exec rm -f {} \;" | crontab -
+crontab -l $USERNAME | echo "* * * * * sudo find /var/www/html/live/ -name '*.ts' -mmin +1 -exec rm -f {} \;" | crontab -
 
 sudo service cron reload
