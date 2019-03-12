623ab327f4787581a664582be8a2ff0a5fb77b1d
stoyanovgeorge <gstoyanov@gmail.com>
2019-03-12 10:08:00 +0000

    completely re-written origin server script and updated README.md file

--- README.md
+++ README.md
@@ -2,9 +2,23 @@
 
 ### Description
 
-I have created a simple bash script how you can create an NGINX origin server where you can store HLS chunks and manifest files from a remote server using the HTTP PUT method. 
+This is a simple bash script how you can create an NGINX origin server where you can store HLS chunks and manifest files from a remote server using the HTTP PUT method. 
 
-My test scenario is a server running ffmpeg creating the HLS chunks and the manifest file pushing the content to a remote web server running NGINX. The script installs automatically NGINX and do the required changes in the NGINX configuration files using the [SED](http://www.grymoire.com/Unix/Sed.html 'SED syntax') command. The name of the script is [origin.sh](https://github.com/stoyanovgeorge/origin_server/blob/master/origin.sh 'Origin Server Creation Script'), you just need to clone it and edit it in order to fit your use case. I have inserted comments where it needs eventually some change from your side. 
+My test scenario is a server running ffmpeg creating the HLS chunks and the manifest file pushing the content to a remote web server running NGINX. The script installs automatically NGINX and do the required changes in the NGINX configuration files using the [SED](http://www.grymoire.com/Unix/Sed.html 'SED syntax') command. The name of the script is [origin.sh](https://github.com/stoyanovgeorge/origin_server/blob/master/origin.sh 'Origin Server Creation Script'), you just need to clone it and run it.
+
+In general the first part of the script is checking if the user input is a valid IP address or netmask and if it is or if it is left empty, it is adapting the `scripts/origin_server` configuration file. The script is having 4 different functions:
+1. `nginx-install` updating the target system and installing the full NGINX package
+2. `dir-creation` - creates the specified in the `origin_server` configuration
+3. `autodelete` - creates a crontab job every minute to check and delete in the defined directories files older than 1 minute.
+4. `nginx_configuration` - which is inserting the specified user IP address or netmask in step #1, copying the `origin_server` configuration to `/etc/nginx/sites-available` and creating a symbolic link of this file in `/etc/nginx/sites-enabled`. It also increases the maximum upload limit to 50Mb and restarts the NGINX in case there aren't any errors in the configuration file, in order to apply the changes. 
+
+Please note that step#3 `autodelete` is optional since most of the packagers are sending HTTP DELETE requests, to delete the old chunks and these requests are respected by the NGINX. 
+
+### Origin_Server Configuration
+
+It sets the location of the NGINX log files to `/var/log/nginx/origin_server` and defines the publish directory to `/var/www/html/live/upload` for live content, `/var/www/html/vod/upload` for VoD content and also the playback URL: `/live/public` for live and `/vod/public` for VoD content. 
+Please note that `/live/public` is actually an alias of `/var/www/html/live/upload`, the same applies for `/vod/public` - alias to `/var/www/html/vod/upload` so `http://hostname/live/upload` should be used as a publish URL and `http://hostname/live/public/` as a playback URL. Similarly `http://hostname/vod/upload` is the publish URL and `http://hostname/vod/public` is the playback URL. 
+The playback URLs are accepting GET requests from all the private networks and also from the initially defined by the user IP address or network. The publish URLs are accepting only PUT and DELETE HTTP requests. 
 
 I have also created a WIKI page where I have explained in details the commands and how you can harden the access to the origin server. So make sure to check my [WIKI](https://github.com/stoyanovgeorge/origin_server/wiki 'Origin Server Wiki').
 
--- origin.sh
+++ origin.sh
@@ -1,73 +1,89 @@
 #!/bin/bash
 
-echo "Please define the IP address or network from which you are going to stream the HLS content followed by / and the network mask. For example 192.168.178.0/24 will allow all IPs from this network, while 192.168.178.100/32 will allow only this particular IP and press [ENTER]:"
+# regex for the IP address validity
+n='([0-9]|[1-9][0-9]|1[0-9][0-9]|2[0-4][0-9]|25[0-5])'
+m='([0-9]|[12][0-9]|3[012])'
 
-read ipadd
+printf "Please define the IP address or network from which you are going to stream the HLS content followed by / and the network mask. For example 192.168.178.0/24 will allow all IPs from this network, while 192.168.178.100/32 will allow only this particular IP. You can also leave it empty and only access from the private IP networks will be allowed.\n\n"
 
-echo "System update"
+# While loop until the user input is a valid IP adress or empty.
+while true
+do
+    IFS= read -rp "IP Address(/Mask): " ipaddr
+    if [[ -z "$ipaddr" ]] || [[ $ipaddr =~ ^$n(\.$n){3}/$m$ ]] || [[ $ipaddr =~ ^$n(\.$n){3}$ ]]; then
+        break
+    else
+        printf "\nPlease enter a valid IP address!\n\n"
+    fi
+done
 
-sudo apt update
-sudo apt upgrade -y
-sudo apt dist-upgrade -y
-sudo apt autoremove -y
+function nginx_install {
+    # Updates and install the full NGINX server
+    echo "System update"
+    sudo apt update 1> /dev/null
+    sudo apt upgrade -y 1> /dev/null
+    sudo apt dist-upgrade -y 1> /dev/null
+    sudo apt autoremove -y 1> /dev/null
+    echo "NGINX Installation with dav_ext_methods support"
+    sudo apt install nginx-full -y 1> /dev/null
+}
 
-echo "NGINX Installation with dav_ext_methods support"
+function dir_creation {
+    echo "Destination directory creation"
+    # You can change here the destination directory. In this script all the chunks and manifest files will be stored in /var/www/html/vod directory.
+    sudo mkdir /var/www/html/vod/upload
+    sudo mkdir /var/www/html/vod/tmp
+    sudo mkdir /var/www/html/live/upload
+    sudo mkdir /var/www/html/live/tmp
+    sudo mkdir /var/log/nginx/origin_server
+    sudo chown -R www-data:www-data /var/www/html/vod /var/www/html/live
+    
+    # Checks if /var/www/html/index.nginx-debian.html exists and if it does, it deletes it.
+    if [ -f /var/www/html/index.nginx-debian.html ];
+    then
+        sudo rm /var/www/html/index.nginx-debian.html
+    fi
+}
 
-sudo apt install nginx-full -y
+function autodelete {
+    # Most of the packagers nowadays have such a built-in function so this is an optional step
+    echo "Creation of auto-delete script and putting it in the crontab"
 
-echo "Destination directory creation"
+    # This script will add a line in the crontab for deletion of files older than 1 minute in the /var/www/html/live directory. 
 
-# You can change here the destination directory. In this script all the chunks and manifest files will be stored in /var/www/html/vod directory.
+    sh scripts/crontab.sh
+}
 
-sudo mkdir /var/www/html/vod
-sudo mkdir /var/www/html/live
-sudo chown -R www-data:www-data /var/www/html/vod /var/www/html/live
+function nginx_configuration {
+    echo "Change of the NGINX configuration"
 
-# sudo rm /var/www/html/index.nginx-debian.html
+    # Here you can change the client_max_body_size to a custom value, it is set to 50Mb, so files larger than 50Mb won't be accepted.
+    sudo sed -i '14i\\tclient_max_body_size 50m;' /etc/nginx/nginx.conf
+    
+    # Make sure to change the allowed IP address to the network / address which will push the cunks. Please refer to the wiki if you need any further information.
+    if [ ! -z "$ipaddr" ];
+    then
+        sed -i "22 a \ \ \ \ \ \ \ \ \ \ \ \ allow $ipaddr;" scripts/origin_server 
+        sed -i "42 a \ \ \ \ \ \ \ \ \ \ \ \ allow $ipaddr;" scripts/origin_server 
+    fi  
+    sudo cp scripts/origin_server /etc/nginx/sites-available
+    sudo ln -s /etc/nginx/sites-available/origin_server /etc/nginx/sites-enabled/origin_server
 
-echo "Creation of auto-delete script and putting it in the crontab"
+    # Checks if the nginx configuration is correct
+    if sudo nginx -t &> /dev/null
+        then
+        echo "NGINX will be restarted"
+        sudo service nginx restart
+        echo "The installation of Origin Server finished successfully"
+    else
+        printf "\nThe NGINX configuration file check failed with the following error:\n\n"
+        sudo nginx -t
+        echo
+    fi
+}
 
-# This script will add a line in the crontab for deletion of files older than 1 minute in the /var/www/html/live directory. 
-
-sh scripts/crontab.sh
-
-echo "Change of the NGINX configuration"
-
-# Here you can change the client_max_body_size to a custom value, it is set to 50Mb, so files larger than 50Mb won't be accepted.
-
-sudo sed -i '14i\\t# File upload size increased by origin.sh script\
-	client_max_body_size 50m;' /etc/nginx/nginx.conf
-
-# Make sure to change the allowed IP address to the network / address which will push the cunks. Please refer to the wiki if you need any further information.
-
-sudo sed -i '49i\\t# Custom block allowing the following WEBDAV methods: HTTP PUT, DELETE, MKCOL and PROPFIND methods only in /vod directory and only for the defined IP/network\
-	\
-	location /vod { \
-                add_header "Access-Control-Allow-Origin"  *;
-                add_header 'Access-Control-Allow-Methods' 'PUT, MKCOL, PROPFIND';
-		dav_methods  PUT MKCOL;\
-		dav_ext_methods PROPFIND OPTIONS;
-                create_full_put_path on;\
-		limit_except  GET HEAD {\
-                        allow '$ipadd';\
-                        deny  all;\
-                }\
-        }\
-	\
-	location /live {\
-		add_header "Access-Control-Allow-Origin"  *;
-                add_header 'Access-Control-Allow-Methods' 'PUT, DELETE, MKCOL, PROPFIND';
-		dav_methods  PUT DELETE MKCOL;\
-		dav_ext_methods PROPFIND OPTIONS;
-                create_full_put_path on;\
-		limit_except  GET HEAD {\
-                        allow '$ipadd';\
-                        deny  all;\
-                }\
-        }' /etc/nginx/sites-available/default
-
-echo "NGINX restart"
-
-sudo service nginx restart
-
-echo "The installation of Origin Server finished successfully"
+# Execution of the main program
+nginx_install
+dir_creation
+autodelete
+nginx_configuration
