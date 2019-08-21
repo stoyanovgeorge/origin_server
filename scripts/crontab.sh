7285f7da73f6a0859aa620ec85039b97028b747e
George Stoyanov <gstoyanov@gmail.com>
2019-08-21 10:53:29 +0200

    Updated crontab.sh
    Added additional comments.

--- scripts/crontab.sh
+++ scripts/crontab.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+# Scheduling a task to remove all the TS files from /var/www/html/live which are older than 1 minute.
 crontab -l $USERNAME | echo "* * * * * sudo find /var/www/html/live/ -name '*.ts' -mmin +1 -exec rm -f {} \;" | crontab -
 
+# Reloading the cron service.
 sudo service cron reload
